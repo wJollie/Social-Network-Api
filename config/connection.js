aa5b19e464b4f4bf864df9448e178699bfbd4259
wjollie <william.jollie23@gmail.com>
2023-09-27 19:25:44 -0400

    -routes created

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/socialNetworkDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/userCotroller");
+
+router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route("/:id/friends/:friendsId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
